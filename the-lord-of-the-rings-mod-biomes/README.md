b3f519bb0a7b7fc85409d51ed14d8c0c6fced2aa
BarchamMal <115943779+BarchamMal@users.noreply.github.com>
2024-07-09 09:53:18 -0500

    Start the biomes part of this project.

---
+++ the-lord-of-the-rings-mod-biomes/README.md
@@ -0,0 +1,5 @@
+# This is the Lord of The Rings Mod: Biomes
+
+In this mod I will be adding crops, saplings, flowers, trees, ores, biomes, etc, which are part of my  LoTR project.
+
+Currently I've added... Nothing. Nope not at all. I'm starting work on it now.
---
+++ the-lord-of-the-rings-mod-biomes/src/client/java/net/barch/lotr/biomes/TheLordOfTheRingsModBiomesClient.java
@@ -0,0 +1,10 @@
+package net.barch.lotr.biomes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheLordOfTheRingsModBiomesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
